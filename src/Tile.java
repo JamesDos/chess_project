4bdda279a89866bf212609a751c02b3ccf29cc79
nelson <nz235@cornell.edu>
2023-09-02 16:23:19 -0400

    tile addition

--- src/Tile.java
+++ src/Tile.java
@@ -1,3 +1,51 @@
-public class Tile {
+public abstract class Tile {
+
+    int tileCords;
+    Tile(int tileCords) {
+        this.tileCords = tileCords;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile {
+        EmptyTile(int cords) {
+            super(cords);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+
+
+
+
+    }
+    public static final class OccupiedTile extends Tile {
+        Piece pieceOnTile;
+
+        OccupiedTile(int tileCords, Piece pieceOnTile) {
+            super(tileCords);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
+
 
 }
